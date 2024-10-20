35e53250890075370a86faeb33d25f99ff47f3f1
PauloHemmel <paulogustavohemmel@gmail.com>
2024-10-19 20:51:50 -0400

    Console.log

---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Paulo Hemmel'); // String = Texto
+console.log("Paulo Hemmel"); // Number = Número, todo nº. é number
+console.log(`'Paulo' "Hemmel"`)
+
+console.log(123456, 15.85444, 35, 15, 'Paulo Hemmel');
