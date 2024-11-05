ce9016e78bd0ec3dd8742767281278da85ebcdeb
PauloHemmel <paulogustavohemmel@gmail.com>
2024-11-05 19:34:41 -0400

    Operadores aritméticos, de atribuição e incremento

---
+++ aula10/index.js
@@ -0,0 +1,41 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação (- / +)
+ * ** Potenciação
+ * % Resto da divisão
+ * 
+ * Precedência:
+ * ()
+ * **
+ * * / %
+ * + -
+ */
+
+// const num1 = 5;
+// const num2 = 5;
+// const num3 = 10;
+
+// não pode incrementar com const
+let contador = 1;
+++contador;
+contador--; //decremento
+contador++; //incremento
+
+
+console.log(contador++);
+console.log(contador);
+
+/** Operadores de atribuição
+ * **=
+ * let contador = 2;
+ * contador **= 10;
+ * console.log(contador);
+ * 
+*/
+
+/**
+ * Conversão de string para: inteiro / float
+ * const num1 = parseInt('5');
+ * const num1 = NUmber('5'); // converte para um número
+ * const num1 = parseFloat('5.2'); 
+ */
