a4c15fba6339c3a901557cee8c845d78cd26fc62
PauloHemmel <paulogustavohemmel@gmail.com>
2024-11-10 19:58:10 -0400

    alert, confirm e prompt (Navegador)

---
+++ aula11/js/script.js
@@ -0,0 +1,14 @@
+//window.alert('Mensagem');
+//window.confirm('Deseja realmente apagar?')
+//window.prompt('Digite seu nome')
+//alert('Olá mundo!')
+//confirm('Deseja apagar:')
+let confirmar = confirm("Realmente deseja apagar?");
+confirmar
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número')
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+console.log(num1 + num2);
+let soma = num1 + num2;
+alert(soma);
